af66e13cbeb4edb04bc821554191a7ffec2a672f
Aniket <rajeshwar9204@gmail.com>
2020-07-28 23:41:57 +0530

    Added stylings in menu

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll", function () {
+  let scroll = scrollY;
+  console.log(scroll);
+  let home_link = document.getElementById("home_link");
+  let about_link = document.getElementById("about_link");
+  let team_link = document.getElementById("team_link");
+  let down_link = document.getElementById("down_link");
+  let contact_link = document.getElementById("contact_link");
+  if (scroll < 605) {
+    home_link.style.color = "black";
+  }
+  if (scroll < 755 && scroll > 557) {
+    about_link.classList.toggle = "active";
+  }
+});
